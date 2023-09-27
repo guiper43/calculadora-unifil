e2b6a0ff6e17be266ceeba6b5ad3be692f79fd13
guiper43 <guilhermemano321@gmail.com>
2023-09-26 23:58:19 -0300

    classe de application adicionada

---
+++ src/main/java/br/calculadoraunifil/CalculadoraApplication.java
@@ -0,0 +1,13 @@
+package br.calculadoraunifil;
+
+import java.util.Scanner;
+
+public class CalculadoraApplication {
+    Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int option;
+
+
+    }
+}
---
+++ src/main/java/br/calculadoraunifil/Main.java
@@ -0,0 +1,7 @@
+package br.calculadoraunifil;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
